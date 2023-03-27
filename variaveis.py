d7d9a3ace1a292cc25e8eb8c956fcb19ecfb1408
Auto Logon <autologon@CSED.LAB>
2023-03-27 16:29:38 -0300

    Adicionado arquivo

--- variaveis.py
+++ variaveis.py
@@ -1,9 +1,9 @@
 a = 5
-b = 7
+b = 4
 soma = a + b
 print(soma)
 
-if soma == 12:
-    print('A soma é 12')
+if soma == 8:
+    print('A soma é 8')
 else:
-    print('A soma não é 12')
+    print('A soma não é 8')
