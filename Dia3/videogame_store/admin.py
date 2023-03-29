2e02970097f9e4602ee6acc79a9de5029a4c12b9
Auto Logon <autologon@CSED.LAB>
2023-03-29 18:19:20 -0300

    Terceiro dia

---
+++ Dia3/videogame_store/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Jogo, Loja
+
+class JogoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco')
+
+admin.site.register(Jogo, JogoAdmin)
+
+class LojaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'endereco', 'telefone')
+
+admin.site.register(Loja, LojaAdmin)
