7b6976d26d4b8993e8c15bf1d4eabc0f0c318689
Eduardo Pereira <eduardopereira@gmail.com>
2023-03-30 22:25:30 -0300

    Terceiro e quarto dia

--- Dia3-Dia4/setup/settings.py
+++ Dia3-Dia4/setup/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'videogame_store',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'Admin',
         'HOST': 'localhost',
         'PORT': '5432'
     }
